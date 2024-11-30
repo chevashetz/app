1b43daf85a7f3352dae7710ef7231ae3206d113d
chevashetz <chevashetz@mail.ru>
2024-11-30 21:16:19 +0300

    add new func

--- components/results.py
+++ components/results.py
@@ -11,12 +11,16 @@ class Results(QWidget):
 
     def setup_ui(self):
         self.group_box: QGroupBox = self.findChild(QGroupBox, 'groupBox')
+        self.btn_go_to_next_page: QPushButton = self.findChild(QPushButton, 'pushButton_next_page')
+        self.btn_go_to_previous_page: QPushButton = self.findChild(QPushButton, 'pushButton_previous_page')
         self.radio_btn_1: QRadioButton = self.group_box.findChild(QRadioButton, 'radioButton_page_1')
         self.radio_btn_2: QRadioButton = self.group_box.findChild(QRadioButton, 'radioButton_page_2')
         self.stackedWidget: QStackedWidget = self.findChild(QStackedWidget, 'stackedWidget')
+        self.stackedWidget2: QStackedWidget = self.findChild(QStackedWidget, 'stackedWidget_2')
 
         self.radio_btn_1.setChecked(True)
         self.stackedWidget.setCurrentIndex(0)
+        self.stackedWidget2.currentChanged.connect(self.on_current_index_changed)
 
         self.radio_btn_1.clicked.connect(self.switch_page_0)
         self.radio_btn_2.clicked.connect(self.switch_page_1)
@@ -26,6 +30,19 @@ class Results(QWidget):
 
     def switch_page_1(self):
         self.stackedWidget.setCurrentIndex(1)
+
+    def on_current_index_changed(self, index):
+        total_pages = self.get_page_count()
+
+        if index == 0:
+            self.btn_go_to_previous_page.setVisible(False)
+        else:
+            self.btn_go_to_previous_page.setVisible(True)
+
+        if index == total_pages - 1:
+            self.btn_go_to_next_page.setVisible(False)
+        else:
+            self.btn_go_to_next_page.setVisible(True)
 
 class ResultsTest(QMainWindow):
     """ТЕСТОВЫЙ КЛАСС, ЧТОБЫ ЗАПУСКАЛАСЬ Result_Tables"""
