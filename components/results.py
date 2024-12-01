4265b8dedc13e8816c2db3cfe4a2099102d05f01
Alexander Panov <palehou@gmail.com>
2024-12-01 18:22:23 +0300

    fix: радиобаттоны работают

--- components/results.py
+++ components/results.py
@@ -1,35 +1,29 @@
 import sys
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QRadioButton, QGroupBox, QLabel, \
-    QStackedWidget
+    QStackedWidget, QButtonGroup
 from config import BASE_DIR
 
 class Results(QWidget):
     def __init__(self, parent=None):
         super(Results, self).__init__(parent)
         uic.loadUi(BASE_DIR / 'results.ui', self, package='components')
+        self.setup_ui()
 
     def setup_ui(self):
-        self.group_box: QGroupBox = self.findChild(QGroupBox, 'groupBox')
+        self.button_group: QButtonGroup = self.findChild(QButtonGroup, 'buttonGroup')  # объединили кнопку в группу
         self.btn_go_to_next_page: QPushButton = self.findChild(QPushButton, 'pushButton_next_page')
         self.btn_go_to_previous_page: QPushButton = self.findChild(QPushButton, 'pushButton_previous_page')
-        self.radio_btn_1: QRadioButton = self.group_box.findChild(QRadioButton, 'radioButton_page_1')
-        self.radio_btn_2: QRadioButton = self.group_box.findChild(QRadioButton, 'radioButton_page_2')
+        self.radio_btn_1: QRadioButton = self.findChild(QRadioButton, 'radioButton_page_1')
+        self.radio_btn_2: QRadioButton = self.findChild(QRadioButton, 'radioButton_page_2')
         self.stackedWidget: QStackedWidget = self.findChild(QStackedWidget, 'stackedWidget')
         self.stackedWidget2: QStackedWidget = self.findChild(QStackedWidget, 'stackedWidget_2')
 
+        self.button_group.buttonClicked.connect(lambda btn: self.stackedWidget.setCurrentIndex(self.button_group.buttons().index(btn)))
         self.radio_btn_1.setChecked(True)
         self.stackedWidget.setCurrentIndex(0)
         self.stackedWidget2.currentChanged.connect(self.on_current_index_changed)
 
-        self.radio_btn_1.clicked.connect(self.switch_page_0)
-        self.radio_btn_2.clicked.connect(self.switch_page_1)
-
-    def switch_page_0(self):
-        self.stackedWidget.setCurrentIndex(0)
-
-    def switch_page_1(self):
-        self.stackedWidget.setCurrentIndex(1)
 
     def on_current_index_changed(self, index):
         total_pages = self.get_page_count()
